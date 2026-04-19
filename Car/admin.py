837878af3fa5777953302da760b833355eb5506d
nazarvojtsehivskiy <nazar.vojtsehivskiy@gmail.com>
2026-04-19 10:33:18 +0300

    laba 3

---
+++ Car/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Brand, Car, Review
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'price', 'created_at', 'updated_at')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('car', 'rating', 'created_at', 'updated_at')
+# Register your models here.
