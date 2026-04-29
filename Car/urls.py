bfe8bddb67103a5489ac23d996f47e5e36ddd698
nazarvojtsehivskiy <nazar.vojtsehivskiy@gmail.com>
2026-04-29 10:58:37 +0300

    laba 5

--- Car/urls.py
+++ Car/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('page1/', views.page1),
-    path('page2/', views.page2),
+    path('', views.home, name='home'),
+    path('category/<int:brand_id>/', views.category, name='category'),
+    path('page1/', views.page1, name='page1'),
+    path('page2/', views.page2, name='page2'),
 ]
--- Car/views.py
+++ Car/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Brand, Car
 
 def home(request):
-    return render(request, 'Car/index.html')
+    brands = Brand.objects.all()
+    cars = Car.objects.select_related('brand').all()
+    return render(request, 'index.html', {'brands': brands, 'cars': cars})
+
+
+def category(request, brand_id: int):
+    selected_brand = get_object_or_404(Brand, pk=brand_id)
+    brands = Brand.objects.all()
+    cars = Car.objects.select_related('brand').filter(brand=selected_brand)
+    return render(
+        request,
+        'Car/category.html',
+        {'brands': brands, 'cars': cars, 'selected_brand': selected_brand},
+    )
 
 def page1(request):
-    return render(request, 'Car/page1.html', {'text': 'Це сторінка 1'})
+    brands = Brand.objects.all()
+    return render(request, 'Car/page1.html', {'text': 'Це сторінка 1', 'brands': brands})
 
 def page2(request):
-    return render(request, 'Car/page2.html', {'text': 'Це сторінка 2'})
+    brands = Brand.objects.all()
+    return render(request, 'Car/page2.html', {'text': 'Це сторінка 2', 'brands': brands})
